172904c2a4a3c2d094357874f8f3a72f86eb43e4
Martin Gao <marting.gao@gmail.com>
2019-03-26 23:36:25 +0800

    update install method

--- android/src/main/java/com/cnull/apkinstaller/ApkInstallerModule.java
+++ android/src/main/java/com/cnull/apkinstaller/ApkInstallerModule.java
@@ -16,6 +16,8 @@ import java.io.*;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import android.os.Bundle;
+import android.app.Activity;
 
 public class ApkInstallerModule extends ReactContextBaseJavaModule {
   private ReactApplicationContext _context = null;
@@ -48,6 +50,33 @@ public class ApkInstallerModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
+  public void getExtra() {
+    Activity activity = _context.getCurrentActivity();
+    Bundle extras = activity.getIntent().getExtras();
+    if (extras != null) {
+      if (extras.containsKey ("path") ) {
+        Log.i("APK_INSTALLER", "path : " + extras.getString ("path"));
+      } 
+
+      if (extras.containsKey ("activity")) {
+        Log.i("APK_INSTALLER", "activity : " + extras.getString ("activity"));
+      }
+    } else {
+    }
+  }
+
+  public execCommand(String cmd) {
+    Process processSU = Runtime.getRuntime().exec(cmd);
+    BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(processSU.getInputStream()));
+    String line;
+    while ((line=bufferedReader.readLine()) != null) {
+      Log.i("APK_INSTALLER", "LOG" + line);
+    }
+    int result = processSU.waitFor();
+    Log.i("APK_INSTALLER", "FINISH " + String.valueOf(result));
+  }
+
+  @ReactMethod
   public void install(String path, String activity) {
     Log.i("APK_INSTALLER", "install " + path);
     String cmd = "chmod 777 " + path;
@@ -58,29 +87,19 @@ public class ApkInstallerModule extends ReactContextBaseJavaModule {
     }
 
     try {
-      String apkCmd = "su pm install -r " + path + " && " + "su am start -n " + activity;
-      Log.i("APK_INSTALLER", apkCmd);
-      ProcessBuilder pb = new ProcessBuilder(apkCmd);
-      pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-      pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-      Process process = pb.start();
-      
+      String installCommand = "su -c pm install -r -d " + path;
+      String restartActivityCommand = "su -c am start -n " + activity;
+      this.execCommand(installCommand);
+      this.execCommand(restartActivityCommand);
     } catch (Exception ex) {
       Log.i("APK_INSTALLER", "ERROR Could not pm install", ex);
     }
-
-
-    // try {
-
-    //   Process processInstall = Runtime.getRuntime().exec(cmd);
-    //   processInstall.waitFor();
-    //   Log.i("APK_INSTALLER", "Finish");
-      
-    // } catch (Exception ex) {
-    //   Log.i("APK_INSTALLER", "ERROR Could not pm install", ex);
-    // }
+    
   }
 }
 
 // pm install -r data/data/com.ruijiahospitalnativev4/cache/com.example.app.1553600357911.apk && am start -n com.ruijiahospitalnativev4/com.ruijiahospitalnativev4.MainActivity
 // am start -n com.ruijiahospitalnativev4/com.ruijiahospitalnativev4.MainActivity
+
+// adb shell am start -a android.settings.SETTINGS && 
+// adb shell am start -n com.ruijiahospitalnativev4/com.ruijiahospitalnativev4.MainActivity -e path data/data/com.ruijiahospitalnativev4/cache/com.example.app.1553600357911.apk -e activity com.ruijiahospitalnativev4/com.ruijiahospitalnativev4.MainActivity
