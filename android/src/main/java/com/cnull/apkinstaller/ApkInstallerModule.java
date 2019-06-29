827bde40f2e01de9b8df04815b503c0aa0f0fc0f
Martin Gao <marting.gao@gmail.com>
2019-06-29 16:52:03 +0800

    update

--- android/src/main/java/com/cnull/apkinstaller/ApkInstallerModule.java
+++ android/src/main/java/com/cnull/apkinstaller/ApkInstallerModule.java
@@ -19,6 +19,8 @@ import android.util.Log;
 import android.os.Bundle;
 import android.app.Activity;
 
+import com.facebook.react.bridge.*;
+
 public class ApkInstallerModule extends ReactContextBaseJavaModule {
   private ReactApplicationContext _context = null;
 
@@ -50,34 +52,110 @@ public class ApkInstallerModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void getExtra() {
+  public void getExtraFromActivity(Promise promise) {
     Activity activity = _context.getCurrentActivity();
     Bundle extras = activity.getIntent().getExtras();
+    String path = "";
+    String targetActivity = "";
     if (extras != null) {
       if (extras.containsKey ("path") ) {
-        Log.i("APK_INSTALLER", "path : " + extras.getString ("path"));
+        path = extras.getString("path");
+        Log.i("APK_INSTALLER", "path : " + extras.getString("path"));
       } 
-
       if (extras.containsKey ("activity")) {
-        Log.i("APK_INSTALLER", "activity : " + extras.getString ("activity"));
+        targetActivity = extras.getString("activity");
+        Log.i("APK_INSTALLER", "activity : " + extras.getString("activity"));
       }
-    } else {
+    }
+    WritableMap map = new WritableNativeMap();
+    map.putString("path", path);
+    map.putString("activity", targetActivity);
+    promise.resolve(map);
+  }
+
+  public void execCommandSecond(String cmd) {
+    // su -c
+    // String finalCmd = "su -c \"" + cmd +"\"";
+    String finalCmd = "su -s sh -c /data/data/com.ruijiahospitalnativev4/cache/test.sh";
+    // Log.i("APK_INSTALLER", "execCommandSecond cmd -> " + cmd);
+    Log.i("APK_INSTALLER", "execCommandSecond finalCmd -> " + finalCmd);
+    try {
+      Process processSU = Runtime.getRuntime().exec(finalCmd);
+      
+      String data = null;
+
+      BufferedReader ie = new BufferedReader(new InputStreamReader(processSU.getErrorStream()));
+      BufferedReader in = new BufferedReader(new InputStreamReader(processSU.getInputStream()));
+      
+      String error = null;
+      while ((error = ie.readLine()) != null && !error.equals("null")) { data += error + "\n"; }
+      String line = null;
+      while ((line = in.readLine()) != null && !line.equals("null")) { data += line + "\n"; }
+      Log.i("APK_INSTALLER", "data ->  " + data);
+
+      int result = processSU.waitFor();
+      Log.i("APK_INSTALLER", "FINISH " + String.valueOf(result));
+      if (result == 0) { Log.i("APK_INSTALLER", "FINISH OS error code 0: Success"); }
+      else if (result == 1) { Log.i("APK_INSTALLER", "FINISH OS error code 1: Operation not permitted"); }
+      else { Log.i("APK_INSTALLER", "FINISH 其他错误"); }
+    } catch (Exception ex) {
+      Log.i("APK_INSTALLER", "ERROR Could not execCommand", ex);
     }
   }
 
-  public execCommand(String cmd) {
-    Process processSU = Runtime.getRuntime().exec(cmd);
-    BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(processSU.getInputStream()));
-    String line;
-    while ((line=bufferedReader.readLine()) != null) {
-      Log.i("APK_INSTALLER", "LOG" + line);
+  public void execCommand(String cmd) {
+    // su -c\
+    Log.i("APK_INSTALLER", "execCommand origin -> " + cmd);
+    String[] cmdArr = new String[] { "su", "-c", cmd };
+    // String finalCmd = String.join(" ", cmdArr);
+    // Log.i("APK_INSTALLER", "execCommand finalCmd -> " + finalCmd);
+    try {
+      
+      // Process processSU = Runtime.getRuntime().exec(finalCmd);
+      Process processSU = Runtime.getRuntime().exec(cmdArr);
+
+      String data = null;
+
+      BufferedReader ie = new BufferedReader(new InputStreamReader(processSU.getErrorStream()));
+      BufferedReader in = new BufferedReader(new InputStreamReader(processSU.getInputStream()));
+      
+      String error = null;
+      while ((error = ie.readLine()) != null && !error.equals("null")) { data += error + "\n"; }
+      String line = null;
+      while ((line = in.readLine()) != null && !line.equals("null")) { data += line + "\n"; }
+      Log.i("APK_INSTALLER", "data ->  " + data);
+      
+      int result = processSU.waitFor();
+      Log.i("APK_INSTALLER", "FINISH " + String.valueOf(result));
+      if (result == 0) { Log.i("APK_INSTALLER", "FINISH OS error code 0: Success"); }
+      else if (result == 1) { Log.i("APK_INSTALLER", "FINISH OS error code 1: Operation not permitted | About to retry"); this.execCommandSecond(cmd); }
+      else { Log.i("APK_INSTALLER", "FINISH 其他错误"); }
+    } catch (Exception ex) {
+      Log.i("APK_INSTALLER", "ERROR Could not execCommand", ex);
     }
-    int result = processSU.waitFor();
-    Log.i("APK_INSTALLER", "FINISH " + String.valueOf(result));
   }
 
   @ReactMethod
-  public void install(String path, String activity) {
+  public void installWithoutCallback(String path) {
+    Log.i("APK_INSTALLER", "installWithoutCallback " + path);
+    String cmd = "chmod 777 " + path;
+    try {
+        Runtime.getRuntime().exec(cmd);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    try {
+      String installCommand = "pm install -r -d " + path;
+      this.execCommand(installCommand);
+      Log.i("APK_INSTALLER", "installWithoutCallback " + "Finished. About to Reboot");
+      this.execCommand("reboot");
+    } catch (Exception ex) {
+      Log.i("APK_INSTALLER", "ERROR Could not pm install", ex);
+    }
+  }
+
+  @ReactMethod
+  public void install(String path, String callbackActivity) {
     Log.i("APK_INSTALLER", "install " + path);
     String cmd = "chmod 777 " + path;
     try {
@@ -85,16 +163,37 @@ public class ApkInstallerModule extends ReactContextBaseJavaModule {
     } catch (Exception e) {
         e.printStackTrace();
     }
-
     try {
-      String installCommand = "su -c pm install -r -d " + path;
-      String restartActivityCommand = "su -c am start -n " + activity;
+      String installCommand = "pm install -r -d " + path;
+      String callbackActivityCommand = "am start -n " + callbackActivity;
       this.execCommand(installCommand);
-      this.execCommand(restartActivityCommand);
+      Thread.sleep(30000);
+      Log.i("APK_INSTALLER", "install " + "Finished. About to Reboot");
+      this.execCommand("reboot");
+      // this.execCommand(callbackActivityCommand);
     } catch (Exception ex) {
       Log.i("APK_INSTALLER", "ERROR Could not pm install", ex);
     }
-    
+  }
+
+  @ReactMethod
+  public void openInstaller(String path, String installerActivity, String callbackActivity) {
+    Log.i("APK_INSTALLER", "install " + path);
+    String cmd = "chmod 777 " + path;
+    try {
+        Runtime.getRuntime().exec(cmd);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    try {
+      String pathExtra = "-e path " + path;
+      String callbackActivityExtra = "-e activity " + callbackActivity;
+      String installerActivityCommand = "am start -n " + installerActivity + " " + pathExtra + " " + callbackActivityExtra;
+      // this.execCommand(installCommand);
+      this.execCommand(installerActivityCommand);
+    } catch (Exception ex) {
+      Log.i("APK_INSTALLER", "ERROR Could not pm install", ex);
+    }
   }
 }
 
@@ -103,3 +202,9 @@ public class ApkInstallerModule extends ReactContextBaseJavaModule {
 
 // adb shell am start -a android.settings.SETTINGS && 
 // adb shell am start -n com.ruijiahospitalnativev4/com.ruijiahospitalnativev4.MainActivity -e path data/data/com.ruijiahospitalnativev4/cache/com.example.app.1553600357911.apk -e activity com.ruijiahospitalnativev4/com.ruijiahospitalnativev4.MainActivity
+// adb shell am start -n com.ruijiahospitalnativeinstaller/com.ruijiahospitalnativeinstaller.MainActivity -e path data/data/com.ruijiahospitalnativev4/cache/com.example.app.1553600357911.apk -e activity com.ruijiahospitalnativev4/com.ruijiahospitalnativev4.MainActivity
+
+
+// echo "am start -n com.ruijiahospitalnativeinstaller/com.ruijiahospitalnativeinstaller.MainActivity" > /data/data/com.ruijiahospitalnativev4/cache/haha.sh
+
+// echo "am start -n com.ruijiahospitalnativeinstaller/com.ruijiahospitalnativeinstaller.MainActivity -e path /data/data/com.ruijiahospitalnativev4/cache/com.example.app.1560329820685.apk -e activity com.ruijiahospitalnativev4/com.ruijiahospitalnativev4.MainActivity" > /data/data/com.ruijiahospitalnativev4/cache/test.sh 
